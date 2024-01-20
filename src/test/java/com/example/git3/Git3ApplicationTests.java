a70a5e0878b2db4c0b49bfd5ba3ecfbe34eb2886
nvko <ksawery.hasnik@gmail.com>
2024-01-20 15:30:36 +0100

    init

---
+++ src/test/java/com/example/git3/Git3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
