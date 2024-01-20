78fcdbff98b74bac438e67e8ca5abe33fee94ad7
nvko <ksawery.hasnik@gmail.com>
2024-01-20 16:34:39 +0100

    TEST-001 Add fixes to controllers

--- src/main/java/com/example/git3/HelloController.java
+++ src/main/java/com/example/git3/HelloController.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HolaController {
+public class HelloController {
 
     @GetMapping
-    public String getHola() {
-        return "Hola!";
+    public String getHello() {
+        return "Hello";
     }
+
 }
